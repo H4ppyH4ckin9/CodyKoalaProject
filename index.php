75daf9280b679f07a85ea7d564f22bc6a98885c7
H4ppyH4ckin9 <44245081+H4ppyH4ckin9@users.noreply.github.com>
2022-03-19 00:12:26 +0100

    update
    navbar hat eigenes Hintergrundbild
    Farbe der Navbar Items jz besser lesbar

--- index.php
+++ index.php
@@ -5,18 +5,20 @@
 			<meta charset="UTF-8">
 			<meta http-equiv="X-UA-Compatible" content="IE=edge">
 			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-			<!--<link rel="stylesheet" href="styling/style.css" />!-->
+			<link rel="stylesheet" href="style.css" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+			<style>
+				body {
+				  background-image: url('jungleBACK.gif');
+				  background-size: cover;
+				}
+			</style>
 	</head>
 <body>
 	<?php include 'navbar.php';?>
 	<h1>Test-Index</h1>
-	<style>
-body {
-  background-image: url('jungleBACK.gif');
-  background-size: cover;
-}
-</style>
+	
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
--- navbar.php
+++ navbar.php
@@ -1,12 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color:#00A333; padding-left:20px;">
+<nav class="navbar navbar-expand-lg navbar-light" id="navigation">
   <a class="navbar-brand" href="#" ><img src="Cody_Icon.png" alt="index.php"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Log-IN</a>
-      <a class="nav-item nav-link active" href="#">Impressum</a>
+      <a class="nav-item nav-link active" href="#" id="NavbarItemColor">Log-IN</a>
+      <a class="nav-item nav-link active" href="#" id="NavbarItemColor">Impressum</a>
      
     </div>
   </div>
