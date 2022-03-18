ec7a81065c55bab713e16e8790f7992fd54e43e4
FyahBender <jhenz@gmx.at>
2022-03-18 17:06:30 +0100

    Cody Icon hinzugefügt

--- navbar.php
+++ navbar.php
@@ -1,12 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color:#00A333;">
-  <a class="navbar-brand" href="#">CodyBild</a>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:#00A333; padding-left:20px;">
+  <a class="navbar-brand" href="#" ><img src="Cody_Icon.png" alt="index.php"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link " href="#"><h3>Log-IN</h3></a>
-      <a class="nav-item nav-link " href="#"><h3>Impressum</h3></a>
+      <a class="nav-item nav-link active" href="#">Log-IN</a>
+      <a class="nav-item nav-link active" href="#">Impressum</a>
      
     </div>
   </div>
