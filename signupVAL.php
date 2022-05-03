64c9d5bf2df4b9f212100034df4eae5854d8579e
FyahBender <jhenz@gmx.at>
2022-05-03 12:52:59 +0200

    Browser Icon für alle Seiten hinzugefügt

--- signupVAL.php
+++ signupVAL.php
@@ -52,6 +52,7 @@ $db_obj->close();
 			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 			<link rel="stylesheet" href="style.css" />
 			<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+			<link rel="icon" href="Cody_Icon.png">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	</head>
